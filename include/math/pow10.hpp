97c3a07cad2c543a02033a77b7829ab8cc95019a
Brandon Lyons <t.brandon.lyons@gmail.com>
2016-11-17 22:58:35 -0500

    Created stand-alone pow10 function

---
+++ include/math/pow10.hpp
@@ -0,0 +1,14 @@
+#ifndef MATH_POW10_HPP
+#define MATH_POW10_HPP
+
+#include <cstdint>
+
+namespace math {
+
+inline constexpr int64_t pow10(uint32_t exponent) {
+   return (exponent == 0) ? 1 : (10 * pow10(exponent - 1));
+}
+
+} // namespace math
+
+#endif // MATH_POW10_HPP
